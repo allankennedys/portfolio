e46cf090ac934607b819d57985c8f0a91bc3c788
allankennedys <kennedyyemail@gmail.com>
2024-12-23 01:57:07 -0300

    commit

---
+++ scroll.js
@@ -0,0 +1,7 @@
+ScrollReveal({reset : true}) ;
+
+
+ScrollReveal().reveal('.navbar', {delay:100, reset:false}) ;
+ScrollReveal().reveal('#home', {delay:100}) ;
+ScrollReveal().reveal('.liclass', {delay:200}) ;
+
