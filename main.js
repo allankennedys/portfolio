d16af14b11d9e945e86c7c0bb548ffe90738f8f9
allankennedys <kennedyyemail@gmail.com>
2024-12-28 01:54:02 -0300

    Add Projetos

--- main.js
+++ main.js
@@ -36,3 +36,6 @@ function nameColorChange(){
     
 }
 nameColorChange();
+
+//formspree
+
--- scroll.js
+++ scroll.js
@@ -3,5 +3,6 @@ ScrollReveal({reset : true}) ;
 
 ScrollReveal().reveal('.navbar', {delay:100, reset:false}) ;
 ScrollReveal().reveal('#home', {delay:100}) ;
+ScrollReveal().reveal('#projects', {delay:100}) ;
 ScrollReveal().reveal('.liclass', {delay:200}) ;
 
